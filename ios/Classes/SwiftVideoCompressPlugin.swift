88516f88c91392d53d14d25a16275ed5ac5c0bd9
MitsuhideOhi <pushumitu@gmail.com>
2022-03-01 22:51:21 +0900

    Fix

--- ios/Classes/SwiftVideoCompressPlugin.swift
+++ ios/Classes/SwiftVideoCompressPlugin.swift
@@ -65,7 +65,7 @@ public class SwiftVideoCompressPlugin: NSObject, FlutterPlugin {
         assetImgGenerate.appliesPreferredTrackTransform = true
         
         let timeScale = CMTimeScale(track.nominalFrameRate)
-        let time = CMTime(position, 1000)
+        let time = CMTime(Float64(truncating: position),preferredTimescale: 1000)
         guard let img = try? assetImgGenerate.copyCGImage(at:time, actualTime: nil) else {
             return nil
         }
